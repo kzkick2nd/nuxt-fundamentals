73abb3cdd3bd200134dcb6bdc7f18d27f71d000f
kzkick2nd <aruga.kazuki@gmail.com>
2020-04-08 00:44:28 +0900

    Dynamic Routes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,116 +1,59 @@
 <template>
-  <div class='container'>
-    <TheNavbar/>
-    <nuxt/>
+  <div class="container">
+    <article>
+      <h1 class="title">{{post.title}}</h1>
+      <p>{{post.content}}</p>
+    </article>
   </div>
 </template>
 
 <script>
-  import TheNavbar from '@/components/TheNavbar'
   export default {
-    components: {
-      TheNavbar
+    data () {
+      return {
+        id: this.$route.params.id,
+        posts: [
+          {
+            id: 'balut',
+            title: 'What is Balut?',
+            content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
+          },
+          {
+            id: 'whereIsIt',
+            title: 'Where is the sign in button??',
+            content: 'I\'ve stalked the forum for a while, reading everything closely, but today I decided that it was time to register as a user. So I did, this morning! But now, when I came back to the forum, I cannot seem to find the damn sign in button? WHERE IS IT???!??'
+          },
+          {
+            id: 'how',
+            title: 'How can I fix this problem?',
+            content: 'Please help me. I dont know what is the best way to fix my problem. I cant reproduce and I cant tell more info. Can someone help me asap??? I am in a hurry!!!'
+          }
+        ]
+      }
+    },
+    computed: {
+      post () {
+        return this.posts.find(post => post.id === this.id)
+      }
     }
   }
 </script>
 
-<style>
-  :root {
-    --navy: #112F41;
-    --teal: #068587;
-    --teal-light: #4FB99F;
-    --yellow: #F2B134;
-    --main-bg-color: coral;
-  }
-
-  html {
-    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-    margin: 0;
-  }
-
-
-  body {
-    padding: 1rem;
-    background: var(--navy);
-    color: white;
-  }
-
-  a {
-    color: var(--teal-light);
-    text-decoration: none;
-    transition: all 0.3s ease;
-  }
-
-  a:hover {
-    color: var(--teal);
-  }
-
+<style scoped>
   .container {
-    margin: 0 auto;
-    max-width: 900px;
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
   }
-
-  .text-xs {
-    font-size: 1.2rem;
+  article * {
+    margin-bottom: 1rem;
   }
-  .text-sm {
-    font-size: 1.4rem;
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
   }
-
-  .text-base {
-    font-size: 1.6rem;
-  }
-
-  .text-lg {
-    font-size: 1.8rem;
-  }
-
-  .text-xl {
+  .title {
     font-size: 2rem;
   }
-
-  h1,h2,h3,h4 {
-    color: var(--yellow);
-  }
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
