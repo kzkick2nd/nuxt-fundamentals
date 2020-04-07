e8f8601f4335692c8f6254fd3180779e0de0564f
kzkick2nd <aruga.kazuki@gmail.com>
2020-04-08 01:00:43 +0900

    Linking Between Pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,59 +1,46 @@
 <template>
   <div class="container">
-    <article>
-      <h1 class="title">{{post.title}}</h1>
-      <p>{{post.content}}</p>
-    </article>
+    <TheNavbar/>
+    <nuxt/>
   </div>
 </template>
 
 <script>
+  import TheNavbar from '@/components/TheNavbar'
   export default {
-    data () {
-      return {
-        id: this.$route.params.id,
-        posts: [
-          {
-            id: 'balut',
-            title: 'What is Balut?',
-            content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
-          },
-          {
-            id: 'whereIsIt',
-            title: 'Where is the sign in button??',
-            content: 'I\'ve stalked the forum for a while, reading everything closely, but today I decided that it was time to register as a user. So I did, this morning! But now, when I came back to the forum, I cannot seem to find the damn sign in button? WHERE IS IT???!??'
-          },
-          {
-            id: 'how',
-            title: 'How can I fix this problem?',
-            content: 'Please help me. I dont know what is the best way to fix my problem. I cant reproduce and I cant tell more info. Can someone help me asap??? I am in a hurry!!!'
-          }
-        ]
-      }
-    },
-    computed: {
-      post () {
-        return this.posts.find(post => post.id === this.id)
-      }
+    components: {
+      TheNavbar
     }
   }
 </script>
 
-<style scoped>
-  .container {
-    display: flex;
-    justify-content: space-between;
-    line-height: 1.5;
-  }
-  article * {
-    margin-bottom: 1rem;
-  }
-  aside {
-    min-width: 280px;
-    max-width: 280px;
-    padding-left: 2rem;
-  }
-  .title {
-    font-size: 2rem;
-  }
+<style>
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
 </style>
